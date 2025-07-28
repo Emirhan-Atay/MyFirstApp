d7f3a3d98cde38b9113e251a5173cc2ad59705c2
Emirhan Atay <emirhan_kkf@outlook.com>
2025-07-28 11:02:40 +0300

    Style_Update

--- src/screens/CategoryListScreen.tsx
+++ src/screens/CategoryListScreen.tsx
@@ -307,8 +307,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logo: {
-    width: 40,
-    height: 40,
+    width: 60,
+    height: 60,
     resizeMode: 'contain',
   },
   searchContainer: {
